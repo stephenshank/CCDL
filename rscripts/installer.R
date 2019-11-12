d7ffcbb33e942cecf84bcc1fa7090980f6114b1c
Jordan Zehr <jordan.zehr@temple.edu>
2019-11-12 17:03:02 -0500

    updated R install script

--- rscripts/installer.R
+++ rscripts/installer.R
@@ -3,4 +3,4 @@ BiocManager::install("ensembldb")
 BiocManager::install("EnsDb.Hsapiens.v86")
 
 
-install.packages("tximport", ,repos = "http://cran.us.r-project.org")
+install.packages("tximport", repos = "http://cran.us.r-project.org")
